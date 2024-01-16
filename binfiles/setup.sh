6957fddf8a3d5736f219d1ed68261f7646f62693
tim0n3 <33823820+tim0n3@users.noreply.github.com>
2024-01-16 21:33:49 +0200

    Update setup.sh

--- binfiles/setup.sh
+++ binfiles/setup.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-dir=~/lazy_bash
+dir=./lazy_bash
 
 function fetchrepo() {
 # Clone git repo
-git clone https://github.com/tim0n3/lazy-bash.git
-# move into lazy-bash dir
-cd lazy-bash
+git clone https://github.com/tim0n3/lazy-bash.git && cd lazy-bash
+sleep 2
 # copy libraries to the user's home folder
 cp -R bash_aliases ~/
+sleep 2
 # overwrite the current .bashrc and load the new shell features
 cat bashrc > ~/.bashrc && source ~/.bashrc
+sleep 2
 # Go $Home and reload the shell
 cd ~/
-clear && reloadbash
+reloadbash
 }
 
 function cleanup() {
@@ -26,10 +27,12 @@ rm -rf ~/lazy-bash
 if [ -d ! ./bash_aliases ]; then
 # Directory exists
 fetchrepo;
+sleep 2
 else
 # Directory doesn't exist
 echo -e "Preparing environment\n"
 cleanup;
+sleep 2
 echo -e "Fetching repo and installing shell overhaul.\n"
 fetchrepo
 fi
