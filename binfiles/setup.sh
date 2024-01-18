bdb492c804e25bd1445685178f4fec01035407a9
tim0n3 <33823820+tim0n3@users.noreply.github.com>
2024-01-18 15:07:08 +0200

    Update setup.sh
    apply my neovim indent defaults

--- binfiles/setup.sh
+++ binfiles/setup.sh
@@ -3,36 +3,36 @@
 dir=./lazy_bash
 
 function fetchrepo() {
-# Clone git repo
-git clone https://github.com/tim0n3/lazy-bash.git && cd lazy-bash
-sleep 2
-# copy libraries to the user's home folder
-cp -R bash_aliases ~/
-sleep 2
-# overwrite the current .bashrc and load the new shell features
-cat bashrc > ~/.bashrc && cd ~/
-sleep 2
-# Go $Home and reload the shell
-source ./.bashrc
-reloadbash
+	# Clone git repo
+	git clone https://github.com/tim0n3/lazy-bash.git && cd lazy-bash
+	sleep 2
+	# copy libraries to the user's home folder
+	cp -R bash_aliases ~/
+	sleep 2
+	# overwrite the current .bashrc and load the new shell features
+	cat bashrc > ~/.bashrc && cd ~/
+	sleep 2
+	# Go $Home and reload the shell
+	source ./.bashrc
+	reloadbash
 }
 
 function cleanup() {
-# Remove detected folders with same name as git repo
-echo -e "Cleaning up left overs\n"
-rm -rf ~/lazy-bash
+	# Remove detected folders with same name as git repo
+	echo -e "Cleaning up left overs\n"
+	rm -rf ~/lazy-bash
 }
 
 #
 if [ -d ! ./bash_aliases ]; then
-# Directory exists
-fetchrepo;
-sleep 2
+	# Directory exists
+	fetchrepo;
+	sleep 2
 else
-# Directory doesn't exist
-echo -e "Preparing environment\n"
-cleanup;
-sleep 2
-echo -e "Fetching repo and installing shell overhaul.\n"
-fetchrepo
+	# Directory doesn't exist
+	echo -e "Preparing environment\n"
+	cleanup;
+	sleep 2
+	echo -e "Fetching repo and installing shell overhaul.\n"
+	fetchrepo
 fi
