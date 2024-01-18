c902ac67bbf97c652e93b78db5e8b156a8db333a
tim0n3 <33823820+tim0n3@users.noreply.github.com>
2024-01-18 15:44:08 +0200

    Update setup.sh
    lots of fixes and quit on error at certain points.

--- binfiles/setup.sh
+++ binfiles/setup.sh
@@ -1,48 +1,52 @@
 #!/bin/bash
 
 dir="$HOME/lazy_bash"
+aliasdir="$HOME/bash_aliases"
 
 function fetchrepo() {
 	# Clone git repo
 	echo -e "Preparing environment\n"
-	sleep 2
+	sleep 1
 	echo -e "Fetching repo and installing shell overhaul.\n"
-	git clone https://github.com/tim0n3/lazy-bash.git && cd lazy-bash
-}	
+	sleep 1
+	git clone https://github.com/tim0n3/lazy-bash.git "$dir" && cd "$dir" || exit 1
+}
 
 function installdeps() {
-	# copy libraries to the user's home folder
-	cp -R bash_aliases ~/
+	# Copy libraries to the user's home folder
+	cp -R "$dir/bash_aliases" "$HOME/"
 
-	# overwrite the current .bashrc and load the new shell features
-	cat bashrc > ~/.bashrc && cd ~/
+	# Overwrite the current .bashrc and load the new shell features
+	cat "$dir/bashrc" > "$HOME/.bashrc" && cd "$HOME" || exit 1
 }
 
 function reloadshell() {
 	# Go $Home and reload the shell
-	source ~/.bashrc
-	reloadbash
+	source "$HOME/.bashrc"
 }
 
-
 function cleanup() {
-	# Remove detected folders with same name as git repo
-	echo -e "Cleaning up left overs\n"
-	rm -rf ~/lazy-bash
+	# Remove detected folders with the same name as git repo
+	echo -e "Cleaning up leftovers\n"
+	rm -rf "$dir" "$aliasdir"
 }
 
 # Main function
-# Check ~/bash_aliases dir exists and cleaup else proceed \
+# Check ~/bash_aliases dir exists and cleanup else proceed \
 # with a normal installation:
-if [ -d ! "$HOME/bash_aliases" ]; then
+if [ -d "$HOME/bash_aliases" ]; then
 	# Directory exists == redownload
-	cleaup
+	cleanup
 	fetchrepo
 	installdeps
 	reloadshell
+	cleanup
 	sleep 2
 else
 	# Directory doesn't exist == business as usual
 	fetchrepo
+	installdeps
+	reloadshell
 	cleanup
 fi
+
