b552d3df0027b8919779af9041d062dffb04eea7
tim0n3 <33823820+tim0n3@users.noreply.github.com>
2024-01-18 15:25:31 +0200

    Update setup.sh
    fix some expansion bugs and making the main function cleaner

--- binfiles/setup.sh
+++ binfiles/setup.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-dir=./lazy_bash
+dir="$HOME/lazy_bash"
 
 function fetchrepo() {
 	# Clone git repo
-	git clone https://github.com/tim0n3/lazy-bash.git && cd lazy-bash
+	echo -e "Preparing environment\n"
 	sleep 2
+	echo -e "Fetching repo and installing shell overhaul.\n"
+	git clone https://github.com/tim0n3/lazy-bash.git && cd lazy-bash
+}	
+
+function installdeps() {
 	# copy libraries to the user's home folder
 	cp -R bash_aliases ~/
-	sleep 2
+
 	# overwrite the current .bashrc and load the new shell features
 	cat bashrc > ~/.bashrc && cd ~/
-	sleep 2
+}
+
+function reloadshell() {
 	# Go $Home and reload the shell
-	source ./.bashrc
+	source ~/.bashrc
 	reloadbash
 }
+
 
 function cleanup() {
 	# Remove detected folders with same name as git repo
@@ -23,16 +31,18 @@ function cleanup() {
 	rm -rf ~/lazy-bash
 }
 
-#
-if [ -d ! ./bash_aliases ]; then
-	# Directory exists
-	fetchrepo;
+# Main function
+# Check ~/bash_aliases dir exists and cleaup else proceed \
+# with a normal installation:
+if [ -d ! "$HOME/bash_aliases" ]; then
+	# Directory exists == redownload
+	cleaup
+	fetchrepo
+	installdeps
+	reloadshell
 	sleep 2
 else
-	# Directory doesn't exist
-	echo -e "Preparing environment\n"
-	cleanup;
-	sleep 2
-	echo -e "Fetching repo and installing shell overhaul.\n"
+	# Directory doesn't exist == business as usual
 	fetchrepo
+	cleanup
 fi
