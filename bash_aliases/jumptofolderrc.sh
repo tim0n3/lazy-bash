9e8760f6f55e72a0eea4018408b6151b207bee63
tim0n3 <33823820+tim0n3@users.noreply.github.com>
2024-08-15 22:03:56 +0200

    feat: dir aliases

--- bash_aliases/jumptofolderrc.sh
+++ bash_aliases/jumptofolderrc.sh
@@ -2,3 +2,18 @@
 #
 # This will be the function that performs and autojump to a directory when \
 # calling a shortcut eg j or hop foldername
+
+
+# Change directory aliases
+alias home='cd ~'
+alias cd..='cd ..'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+
+# cd into the old directory
+alias bd='cd "$OLDPWD"'
+
+# Remove a directory and all files
+alias rmd='rm  -rf --verbose '
