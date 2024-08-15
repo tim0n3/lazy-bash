a3bf4eebed6f2409bf4e9c089d006c869bb02a4b
tim0n3 <33823820+tim0n3@users.noreply.github.com>
2024-08-15 22:04:20 +0200

    feat: added copy progress bar

--- bash_aliases/jumptofolderrc.sh
+++ bash_aliases/jumptofolderrc.sh
@@ -17,3 +17,24 @@ alias bd='cd "$OLDPWD"'
 
 # Remove a directory and all files
 alias rmd='rm  -rf --verbose '
+
+# Copy file with a progress bar
+copy()
+{
+	set -e
+	strace -q -ewrite cp -- "${1}" "${2}" 2>&1 \
+	| awk '{
+	count += $NF
+	if (count % 10 == 0) {
+		percent = count / total_size * 100
+		printf "%3d%% [", percent
+		for (i=0;i<=percent;i++)
+			printf "="
+			printf ">"
+			for (i=percent;i<100;i++)
+				printf " "
+				printf "]\r"
+			}
+		}
+	END { print "" }' total_size=$(stat -c '%s' "${1}") count=0
+}
